420a7b350d03018f01ddd8602d2dd81815dda6bd
Nana-4gohil <gohilnana9@gmail.com>
2025-07-05 16:36:10 +0530

    Implement two number sum

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,9 +1,13 @@
 class StringCalculator {
 
     Add(numbers) {
-    if (!numbers) return 0;
-    return parseInt(numbers);
-  }
+        if (!numbers) return 0;
+        if (numbers.includes(',')) {
+            const numArray = numbers.split(',').map(num => parseInt(num));
+            return numArray.reduce((sum, num) => sum + num, 0);
+        }
+        return parseInt(numbers);
+    }
 }
 
 module.exports = StringCalculator;
