e0346e8c89c20b0298b590bc06d37103e771e6fe
Nana-4gohil <gohilnana9@gmail.com>
2025-07-05 16:39:42 +0530

    Implement new line delimiter support

--- StringCalculator.js
+++ StringCalculator.js
@@ -2,7 +2,8 @@ class StringCalculator {
 
     Add(numbers) {
         if (!numbers) return 0;
-        const numArray = numbers.split(',').map(num => parseInt(num)).filter(num => !isNaN(num));
+        const numberString = numbers.replace(/\n/g, ',');
+        const numArray = numberString.split(',').map(num => parseInt(num)).filter(num => !isNaN(num));
         return numArray.reduce((sum, num) => sum + num, 0);
     }
 }
