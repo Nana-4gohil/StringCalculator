e631720aea0c402e1d22ce148b88db9dbf20d6d1
Nana-4gohil <gohilnana9@gmail.com>
2025-07-05 16:41:29 +0530

    Implement custom delimiter support

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,11 +1,16 @@
 class StringCalculator {
-
     Add(numbers) {
         if (!numbers) return 0;
-        const numberString = numbers.replace(/\n/g, ',');
-        const numArray = numberString.split(',').map(num => parseInt(num)).filter(num => !isNaN(num));
+        let delimiter = ',';
+        let numberString = numbers;
+        if (numbers.startsWith('//')) {
+            const parts = numbers.split('\n');
+            delimiter = parts[0].slice(2);
+            numberString = parts[1];
+        }
+        numberString = numberString.replace(/\n/g, delimiter);
+        const numArray = numberString.split(delimiter).map(num => parseInt(num)).filter(num => !isNaN(num));
         return numArray.reduce((sum, num) => sum + num, 0);
     }
 }
-
 module.exports = StringCalculator;
